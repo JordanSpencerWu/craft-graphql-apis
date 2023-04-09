e45ac213d3813db4cc5aa44a8f2c38584570c8d2
Jordan Wu <jordanwu@Jordans-MacBook-Pro-4.local>
2023-04-09 13:10:57 -0600

    add chapter 4 adding flexibility

--- server/lib/plate_slate/menu.ex
+++ server/lib/plate_slate/menu.ex
@@ -4,9 +4,10 @@ defmodule PlateSlate.Menu do
   """
 
   import Ecto.Query, warn: false
-  alias PlateSlate.Repo
 
+  alias PlateSlate.Repo
   alias PlateSlate.Menu.Category
+  alias PlateSlate.Menu.Item
 
   @doc """
   Returns the list of categories.
@@ -233,5 +234,18 @@ defmodule PlateSlate.Menu do
   """
   def change_item(%Item{} = item) do
     Item.changeset(item, %{})
+  end
+
+  @search [Item, Category]
+  def search(term) do
+    pattern = "%#{term}%"
+    Enum.flat_map(@search, &search_ecto(&1, pattern))
+  end
+
+  defp search_ecto(ecto_schema, pattern) do
+    Repo.all(
+      from q in ecto_schema,
+        where: ilike(q.name, ^pattern) or ilike(q.description, ^pattern)
+    )
   end
 end
--- server/lib/plate_slate_web/schema.ex
+++ server/lib/plate_slate_web/schema.ex
@@ -1,17 +1,11 @@
 defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
 
-  alias PlateSlateWeb.Resolvers
-
   import_types(Absinthe.Type.Custom)
+  import_types(__MODULE__.MenuTypes)
 
   query do
-    @desc "The list of available items on the menu"
-    field :menu_items, list_of(:menu_item) do
-      arg(:filter, :menu_item_filter)
-      arg(:order, type: :sort_order, default_value: :asc)
-      resolve(&Resolvers.Menu.menu_items/3)
-    end
+    import_fields(:menu_queries)
   end
 
   enum :sort_order do
@@ -19,41 +13,20 @@ defmodule PlateSlateWeb.Schema do
     value(:desc)
   end
 
-  @desc "Filtering options for the menu item list"
-  input_object :menu_item_filter do
-    @desc "Matching a name"
+  @desc "interface for search result"
+  interface :search_result do
+    @desc "name of search result"
     field :name, :string
 
-    @desc "Matching a category name"
-    field :category, :string
+    resolve_type(fn
+      %PlateSlate.Menu.Item{}, _ ->
+        :menu_item
 
-    @desc "Matching a tag"
-    field :tag, :string
+      %PlateSlate.Menu.Category{}, _ ->
+        :category
 
-    @desc "Priced above a value"
-    field :priced_above, :float
-
-    @desc "Priced below a value"
-    field :priced_below, :float
-
-    @desc "Added to the menu before this date"
-    field :added_before, :date
-
-    @desc "Added to the menu after this date"
-    field :added_after, :date
-  end
-
-  @desc "A item in a menu"
-  object :menu_item do
-    @desc "id"
-    field :id, :id
-    @desc "date of when menu item was added"
-    field :added_on, :date
-    @desc "description of menu item"
-    field :description, :string
-    @desc "name of menu item"
-    field :name, :string
-    @desc "price of menu item"
-    field :price, :decimal
+      _, _ ->
+        nil
+    end)
   end
 end
